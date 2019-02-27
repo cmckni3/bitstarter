a86ae5141cca383cbfe434c57e1e3ce4c26565b9
Chris McKnight <cmckni3@gmail.com>
2019-02-27 14:34:07 -0600

    Update setup-ssjs.sh

--- setup-ssjs.sh
+++ setup-ssjs.sh
@@ -11,7 +11,7 @@ heroku create
 ssh-keygen -t rsa
 heroku keys:add
 heroku addons:add heroku-postgresql:dev
-heroku pg:promote `heroku config  | grep HEROKU_POSTGRESQL | cut -f1 -d':'`
+heroku pg:promote $(heroku config  | grep HEROKU_POSTGRESQL | cut -f1 -d':')
 heroku plugins:install git://github.com/ddollar/heroku-config.git
 
 # Set up heroku configuration variables
